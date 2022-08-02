64104398368b3a42b82af0ad78fbacd657065d01
durveshramane26 <durveshramane6@gmail.com>
2022-08-02 19:18:32 +0530

    design added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+
+
 function App() {
+
+  function emojiInputHandler(event) {}
   return (
     <div className="App">
-      
+      <h1> Inside out </h1>
+      <input onChange={emojiInputHandler}></input>
+      <h2>{}</h2>
     </div>
   );
 }
